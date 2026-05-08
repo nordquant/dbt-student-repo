c10688d872f3dee605e774444b951ceabbb309ed
Andrei Naideshkin <aenaydeshkin@gmail.com>
2026-05-08 17:59:18 +0200

    add src tables

---
+++ airbnb/models/src/src_reviews.sql
@@ -0,0 +1,14 @@
+WITH raw_reviews AS (
+    SELECT
+        *
+    FROM
+        AIRBNB.RAW.RAW_REVIEWS
+)
+SELECT
+    LISTING_ID, 
+    DATE as REVIEW_DATE, 
+    REVIEWER_NAME,
+    COMMENTS as REVIEW_TEXT, 
+    SENTIMENT as REVIEW_SENTIMENT
+FROM
+    raw_reviews
