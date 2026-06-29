e1cade4b50e354d5da8a8462b1c32df82ec68097
Gomathi Sankaranarayanan <ksgomathi@gmail.com>
2026-06-29 15:50:09 +0200

    modifying name of model

--- airbnb/models/dim/dim_long_term_listings.py
+++ airbnb/models/dim/dim_long_term_listings.py
@@ -1,5 +1,5 @@
 def model(dbt, session):
-    listings = dbt.ref("dim_listings_cleansed")
+    listings = dbt.ref("dim_listing_cleansed")
 
     return (listings.filter(listings["MINIMUM_NIGHTS"] >= 30)
                    .select("LISTING_ID", "LISTING_NAME", "PRICE"))
